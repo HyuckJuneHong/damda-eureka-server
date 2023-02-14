5f817156fe3f2a3fe4c8708a8c011bb6b5bb5fb1
홍혁준 <jjj8337721@naver.com>
2023-02-14 13:06:22 +0900

    fix : core module remove

--- src/test/java/kr/co/damdadiscovery/DamdaDiscoveryApplicationTests.java
+++ src/test/java/kr/co/damdadiscovery/DamdaDiscoveryApplicationTests.java
@@ -1,13 +1,7 @@
 package kr.co.damdadiscovery;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DamdaDiscoveryApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
